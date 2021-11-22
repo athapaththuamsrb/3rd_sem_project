f5c836807f080c4f817cf063598952eaff9de397
sasandi99 <sasandi99@hotmail.com>
2021-11-22 08:55:34 +0530

    update_stocks

--- .utils/dbcon.php
+++ .utils/dbcon.php
@@ -255,10 +255,10 @@ class DatabaseConn
         $q = "UPDATE stocks set reserved = reserved + '$amount' WHERE district = ? AND place = ? AND date = ? and type = ? and dose = ?";
       }
       else if ($field === "not_reserved"){
-        $q = "UPDATE stocks set not_reserved = not_reserved + '$amount' district = ? AND place = ? AND date = ? and type = ? and dose = ?";
+        $q = "UPDATE stocks set not_reserved = not_reserved + '$amount' WHERE district = ? AND place = ? AND date = ? and type = ? and dose = ?";
       }
       else{
-        $q = "UPDATE stocks set appointments = appointements + '$amount' WHERE district = ? AND place = ? AND date = ? and type = ? and dose = ?";
+        $q = "UPDATE stocks set appointments = appointments + '$amount' WHERE district = ? AND place = ? AND date = ? and type = ? and dose = ?";
       }
       $stmt = $this->conn->prepare($q);
       $stmt->bind_param("ssssi", $district, $place, $date, $type, $dose);
