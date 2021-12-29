2ba55555010db61a3c4aed848d75ea998074b7a1
athapaththuamsrb <rukshanathapaththu07@gmail.com>
2021-12-29 15:22:12 +0530

    corrected some mistakes in vaccination/addStock.php

--- vaccination/addStock.php
+++ vaccination/addStock.php
@@ -7,14 +7,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if (isset($_POST['date']) && isset($_POST['type']) && isset($_POST['dose']) && isset($_POST['amount']) && isset($_POST['onlineAmount']) && $_POST['date'] && $_POST['type'] && is_numeric($_POST['dose']) && is_numeric($_POST['amount']) && is_numeric($_POST['onlineAmount'])) {
     $district = $user->getDistrict();
     $place = $user->getPlace();
+    $date = new DateTime($_POST['date']);
     $type = $_POST['type'];
     $dose = intval($_POST['dose']);
     $amount = intval($_POST['amount']);
+    $online = intval($_POST['onlineAmount']);
     require_once('../.utils/dbcon.php');
     $con = DatabaseConn::get_conn();
     $success = false;
-    if ($dose > 0 && $amount > 0) {
-      if ($con->filter_vaccine_centres($district, $place, $date, $type, $dose, $amount - $online, $online)) {
+    if ($dose > 0 && $amount > 0 && $online >= 0 && $amount >= $online) {
+      if ($con->add_stock($district, $place, $date, $type, $dose, $amount - $online, $online)) {
         $success = true;
       }
     }
