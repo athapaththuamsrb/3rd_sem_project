440883c5d526c2afc0b63682d378de36cafc0f6f
Thevindu <xxxxxxxxxxx>
2022-01-11 00:09:48 +0530

    bug fix in date in vaccineAvailability.php

--- vaccineAvailability.php
+++ vaccineAvailability.php
@@ -5,18 +5,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $district = $_POST['district'];
     $type = $_POST['type'];
     if ($type != "Pfizer" && $type != "Moderna" && $type != "Sinopharm" && $type != "Aztraseneca") {
+      $data['reason'] = 'Invalid Type';
       echo json_encode($data);
       die();
     }
     $dose = intval($_POST['dose']);
     if ($dose <= 0) {
+      $data['reason'] = 'Invalid Dose';
       echo json_encode($data);
       die();
     }
     try {
       $date = new DateTime($_POST['date']);
-      $now = new DateTime("now");
-      if ($date < $now) {
+      $yesterday = new DateTime('yesterday');
+      if ($date <= $yesterday) {
+        $data['reason'] = 'Invalid Date';
         echo json_encode($data);
         die();
       }
@@ -27,6 +30,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require_once('.utils/dbcon.php');
     if ($con = DatabaseConn::get_conn()) {
       $data = $con->getAvailability($district, $type, $dose, $date);
+    }else{
+      $data['reason'] = 'Server error';
     }
     echo json_encode($data);
   }
